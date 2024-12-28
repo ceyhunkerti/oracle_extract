fb7fae426c292a967920af513c6dccd90bf0e443
Ceyhun Kerti <ceyhun.kerti@gmail.com>
2024-12-28 21:56:40 +0300

    example scripts

--- examples/scripts/run1.sh
+++ examples/scripts/run1.sh
@@ -3,6 +3,9 @@ zig-out/bin/ox run \
     --username sys \
     --password Oracle_123 \
     --auth-mode SYSDBA \
-    --sql "SELECT * FROM sys.table1" \
+    --sql "
+        SELECT *
+        FROM sys.table1
+    " \
     --output-file "tmpdir/run1-output.csv" \
     --fetch-size 10000
