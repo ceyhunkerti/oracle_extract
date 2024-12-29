197cf7a1cec87c7e0e6202f61c419871170abfa3
Ceyhun Kerti <ceyhun.kerti@gmail.com>
2024-12-29 15:26:46 +0300

    zig version check

--- build.zig
+++ build.zig
@@ -1,4 +1,23 @@
 const std = @import("std");
+const builtin = @import("builtin");
+
+comptime {
+    // This is the required Zig version for building this project. We allow
+    // any patch version but the major and minor must match exactly.
+    const required_zig = "0.14.0";
+
+    // Fail compilation if the current Zig version doesn't meet requirements.
+    const current_vsn = builtin.zig_version;
+    const required_vsn = std.SemanticVersion.parse(required_zig) catch unreachable;
+    if (current_vsn.major != required_vsn.major or
+        current_vsn.minor != required_vsn.minor)
+    {
+        @compileError(std.fmt.comptimePrint(
+            "Your Zig version v{} does not meet the required build version of v{}",
+            .{ current_vsn, required_vsn },
+        ));
+    }
+}
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
